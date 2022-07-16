20359e14e3c5b1fbfdeda552c27b2b3e311d1054
Maria Åhlén <93766912+irisitymariaahlen@users.noreply.github.com>
2022-07-16 10:25:55 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0W49_7ySGilQNc8DayzN4l__soM1OH9g",
+  authDomain: "matkalkylatorn.firebaseapp.com",
+  projectId: "matkalkylatorn",
+  storageBucket: "matkalkylatorn.appspot.com",
+  messagingSenderId: "403319282566",
+  appId: "1:403319282566:web:dc87a93b0ea8be4877ea07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
